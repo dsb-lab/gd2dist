795326a31e2f779fa637bf3f9ebfeb7405514bad
Gabriel Torregrosa <g.torregrosa@outlook.com>
2020-09-04 12:21:00 +0200

    Store version before crosscompiler

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="gaussiandeconvolution",
-    version="0.1",
+    version="0.0.1",
     description="A gaussian deconvolution package",
     author='Gabriel Torregrosa Cortés',
     author_email="g.torregrosa@outlook.com",
